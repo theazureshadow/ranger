64df05b8e7a467f3658c4aa84951ab94a25ad6cb
Chris Sullins <theazureshadow@gmail.com>
2013-02-18 22:14:09 -0800

    Refactored root ranges to contain content drivers.
    Added a ContentDriver class for the root to delegate edit/view primitives.
    Also cleaned up whitespace.

--- src/ranger.js
+++ src/ranger.js
@@ -8,15 +8,8 @@ var ranger = (function() {
         ChildClass.prototype = new Between();
         ChildClass.prototype.constructor = ChildClass;
     }
-    /** Copies all of Parent's direct prototype methods to Child's prototype */
-    function extend(ChildClass, ParentClass) {
-        var child = ChildClass.prototype, parent = ParentClass.prototype;
-        for (var i in parent) {
-            if (parent.hasOwnProperty(i) && !child.hasOwnProperty(i)) {
-                child[i] = parent[i];
-            }
-        }
-    }
+
+
     /**
      * Calculates a position offset according to the containing range. Handles
      * negatives as the end, and null or undefined as start
@@ -38,58 +31,54 @@ var ranger = (function() {
     function normalizeEnd(position, length) {
         return normalizePosition(position, length, true);
     }
-    
+
     /** @interface */
-    function BaseRange(content) {}
+    function ContentDriver(content) {}
     /**
      * @param start The start index. If negative, measures from the end. If
      * missing, includes the beginning.
      * @param end The end index. If negative, measures from the end. If missing,
      * includes the end.
      */
-    BaseRange.prototype.slice = function(start, end) {};
-    
+    ContentDriver.prototype.slice = function(start, end) {};
+
     /**
      * @param start The start index.
      * @param end The end index.
      * @param newContent The content with which to replace the old content.
      */
-     BaseRange.prototype.replace = function(start, end, newContent) {};
-     
-     /**
-      * @return {number} The length of this range.
-      */
-     BaseRange.prototype.length = function() {};
-    
-    
-    /** @constructor @implements BaseRange */
-    function SubRange() {}
-    
-    
+    ContentDriver.prototype.replace = function(start, end, newContent) {};
+
+    /**
+     * @return {number} The length of this range.
+     */
+    ContentDriver.prototype.length = function() {};
+
+
     /**
      * Represents a base range of a string.
-     * @implements BaseRange
+     * @implements ContentDriver
      * @constructor
      */
-    function StringRange(content) {
+    function StringContentDriver(content) {
         /** @type {string} */
         this.content = content;
     }
-    
+
     /**
      * Implements the view primitive
      */
-    StringRange.prototype.slice = function(start, end) {
+    StringContentDriver.prototype.slice = function(start, end) {
         var length = this.length();
         start = normalizeStart(start, length);
         end = normalizeEnd(end, length);
         return this.content.substr(start, end - start);
     };
-    
+
     /**
      * Implements the replacement primitive for modifying.
      */
-    StringRange.prototype.replace = function(start, end, newContent) {
+    StringContentDriver.prototype.replace = function(start, end, newContent) {
         var content, oldContent, length, before, during, after;
         length = this.length();
         start = normalizeStart(start, length);
@@ -105,20 +94,47 @@ var ranger = (function() {
         this.content = [before, during, after].join('');
         return oldContent;
     };
-    
-    StringRange.prototype.length = function() {
+
+    StringContentDriver.prototype.length = function() {
         return this.content.length;
     };
-    
-    
+
+
+    /**
+     * @constructor
+     */
+    function RootRange() {}
+
+    RootRange.prototype.slice = function(start, end) {
+        return this.driver.slice(start, end);
+    };
+    RootRange.prototype.replace = function(start, end, content) {
+        return this.driver.replace(start, end, content);
+    };
+
+    /** @type {ContentDriver} */
+    RootRange.prototype.driver = null;
+
+
+    /** @constructor @extends RootRange */
+    function StringRange(string) {
+        this.driver = new StringContentDriver(string);
+    }
+    inherit(StringRange, RootRange);
+
+
+    /** @constructor */
+    function SubRange() {}
+
+
     // Store a reference to the old ranger object
     var oldRanger = ranger;
-    
-    
+
+
     // Represents the top-level Ranger interface
     /** @constructor */
     function Ranger() {}
-    
+
     /**
      * Sets ranger to be the original ranger and returns a reference to this.
      * @return this
@@ -127,11 +143,11 @@ var ranger = (function() {
         ranger = oldRanger;
         return this;
     };
-    
+
     Ranger.prototype.fromString = function(string) {
         return new StringRange(string);
     };
-    
+
     // Export Ranger instance
     return new Ranger();
 }());
